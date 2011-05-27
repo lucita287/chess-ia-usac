5702b326978d0e09d235cb2d20f2f01716da0cba
guele33@gmail.com <guele33@gmail.com@f80c2c3c-c1f3-95a4-9259-38f65a09c78a>
2011-05-27 22:25:17 +0000

    Version 0.3

--- IA1_Proyecto/src/gui/jugador/xypieza.java
+++ IA1_Proyecto/src/gui/jugador/xypieza.java
@@ -6,12 +6,27 @@ package gui.jugador;
  */
 public class xypieza {
 
+    private int pieza;
     private int x;
     private int y;
+
+    public xypieza(int pieza, int y, int x) {
+        this.pieza=pieza;
+        this.x = x;
+        this.y = y;
+    }
 
     public xypieza(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getPieza() {
+        return pieza;
+    }
+
+    public void setPieza(int pieza) {
+        this.pieza = pieza;
     }
 
     public int getX() {
