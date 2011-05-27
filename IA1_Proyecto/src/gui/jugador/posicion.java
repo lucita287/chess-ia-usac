287205584d09ac59a77bc96b3a13bffb88931077
farendaway@gmail.com <farendaway@gmail.com@f80c2c3c-c1f3-95a4-9259-38f65a09c78a>
2011-05-27 15:53:53 +0000

    Clase creada por david 0.2

---
+++ IA1_Proyecto/src/gui/jugador/posicion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui.jugador;
+
+/**
+ *
+ * @author asakur
+ */
+public class posicion {
+
+    int x;
+    int y;
+    public posicion(int x_,int y_){
+        x=x_;
+        y=y_;
+    }
+
+}
